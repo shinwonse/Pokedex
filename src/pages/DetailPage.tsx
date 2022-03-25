69a8514bdd4002f012f85f280a1b3d5383b3a79f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-25 23:51:22 +0900

    feat: :sparkles: 상세페이지 작성

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import PokemonInfo from "../components/PokemonInfo";
+import Tabs from "../components/Tabs";
 
 type Params = {
   id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 const DetailPage: React.FC = () => {
   const { id } = useParams<Params>();
-  return <div>DetailPage id: {id}</div>;
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+  return (
+    <div>
+      <Tabs
+        tab={selectedTab}
+        onClick={handleClick}
+        color={{ name: "red", url: "" }}
+      />
+    </div>
+  );
 };
 
 export default DetailPage;
