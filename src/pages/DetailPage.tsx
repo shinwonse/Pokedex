611395c78b9b66aa24cb16ed3b74ee25805d9b1e
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-26 22:18:34 +0900

    feat: :sparkles: 포켓몬 상세페이지 작성

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,7 +1,23 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
+import About from "../components/About";
+import Evolution from "../components/Evolution";
 import PokemonInfo from "../components/PokemonInfo";
+import Stats from "../components/Stats";
 import Tabs from "../components/Tabs";
+import usePokemon from "../hooks/usePokemon";
+import useSpecies from "../hooks/useSpecies";
+import { PokemonResponse } from "../types";
+import styled from "@emotion/styled/macro";
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TabsWrapper = styled.div`
+  margin: 24px auto 0;
+`;
 
 type Params = {
   id: string;
@@ -13,17 +29,86 @@ const DetailPage: React.FC = () => {
   const { id } = useParams<Params>();
   const [selectedTab, setSelectedTab] = useState<Tab>("about");
 
+  const pokemonResult = usePokemon<PokemonResponse>(id);
+  const speciesResult = useSpecies(id + "");
+
+  const { name, types, height, weight, abilities, baseExp, stats } = useMemo(
+    () => ({
+      name: pokemonResult.data?.data.name,
+      types: pokemonResult.data?.data.types,
+      height: pokemonResult.data?.data.height,
+      weight: pokemonResult.data?.data.weight,
+      abilities: pokemonResult.data?.data.abilities,
+      baseExp: pokemonResult.data?.data.base_experience,
+      stats: pokemonResult.data?.data.stats,
+    }),
+    [pokemonResult]
+  );
+
+  const {
+    color,
+    growthRate,
+    flavorText,
+    genderRate,
+    isLegendary,
+    isMythical,
+    evolutionChainUrl,
+  } = useMemo(
+    () => ({
+      color: speciesResult.data?.data.color,
+      growthRate: speciesResult.data?.data.growth_rate.name,
+      flavorText: speciesResult.data?.data.flavor_text_entries[0].flavor_text,
+      genderRate: speciesResult.data?.data.gender_rate,
+      isLegendary: speciesResult.data?.data.is_legendary,
+      isMythical: speciesResult.data?.data.is_mythical,
+      evolutionChainUrl: speciesResult.data?.data.evolution_chain.url,
+    }),
+    [speciesResult]
+  );
+
   const handleClick = (tab: Tab) => {
     setSelectedTab(tab);
   };
   return (
-    <div>
-      <Tabs
-        tab={selectedTab}
-        onClick={handleClick}
-        color={{ name: "red", url: "" }}
-      />
-    </div>
+    <Container>
+      <PokemonInfo id={id + ""} name={name} types={types} color={color} />
+      <TabsWrapper>
+        <Tabs
+          tab={selectedTab}
+          onClick={handleClick}
+          color={{ name: "red", url: "" }}
+        />
+      </TabsWrapper>
+      {selectedTab === "about" && (
+        <About
+          isLoading={pokemonResult.isLoading || pokemonResult.isLoading}
+          color={color}
+          genderRate={genderRate}
+          isLegendary={isLegendary}
+          isMythical={isMythical}
+          types={types}
+          weight={weight}
+          height={height}
+          baseExp={baseExp}
+          abilities={abilities}
+        />
+      )}
+      {selectedTab === "stats" && (
+        <Stats
+          isLoading={pokemonResult.isLoading || speciesResult.isLoading}
+          color={color}
+          stats={stats}
+        />
+      )}
+      {selectedTab === "evolution" && (
+        <Evolution
+          isLoading={speciesResult.isLoading}
+          id={id}
+          color={color}
+          url={evolutionChainUrl}
+        />
+      )}
+    </Container>
   );
 };
 
