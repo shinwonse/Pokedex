510ca805a4f160bc73cc2118da9ebdcc4f76c782
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-03-26 22:18:09 +0900

    feat: :sparkles: 커스텀 훅 작성

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
-import { useQuery, UseQueryResult } from "react-query";
+import { useQueries, useQuery, UseQueryResult } from "react-query";
+import { PokemonResponse } from "../types";
 
 const pokemonApi = (id?: string) =>
   axios.get(`https://pokeapi.co/api/v2/pokemon/${id || ""}`, {
@@ -10,6 +11,19 @@ const usePokemon = <T>(
   id?: string
 ): UseQueryResult<AxiosResponse<T>, Error> => {
   return useQuery(id ? ["pokemon", id] : "pokemon", () => pokemonApi(id));
+};
+
+export const usePokemonQueries = (
+  names: string[]
+): Array<UseQueryResult<AxiosResponse<PokemonResponse>, Error>> => {
+  const queries = names.map((name, idx) => ({
+    queryKey: ["evolution", `${name}_${idx}`],
+    queryFn: () => pokemonApi(name),
+  }));
+
+  return useQueries(queries) as Array<
+    UseQueryResult<AxiosResponse<PokemonResponse>, Error>
+  >;
 };
 
 export default usePokemon;
